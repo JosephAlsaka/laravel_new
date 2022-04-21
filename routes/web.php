d1ec37bd68fbdf5dab8410dc19f550645bfbb6d2
JosephAlsaka <joseph70.alsaka@gmail.com>
2022-04-21 13:28:38 +0300

    Joseph_2

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes(['verify'=>true]);
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth')->name('verification.notice');;
