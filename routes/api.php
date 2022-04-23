e59abf34cd143df7161a9866c092250b85571755
JosephAlsaka <joseph70.alsaka@gmail.com>
2022-04-23 13:17:20 +0300

    Joseph_5

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-
+Route::get('/hello',function(){
+    return "hello salimo" ;
+});
 Auth::routes(['verify'=>true]);
-
 Route::middleware('auth:api')->group(function(){
     Route::get('/user',function(Request $request){
         return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/hello',function(){
+    return "hello salimo" ;
+});
 Auth::routes(['verify'=>true]);
 Route::get('/', function () {
     return view('welcome');
